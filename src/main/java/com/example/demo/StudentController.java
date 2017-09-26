55360b569953cf93062ad48afc846ad1ee7bd19c
Evan Coakley <evancoakley1@gmail.com>
2017-09-26 18:25:37 -0500

    Thymeleaf done

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -3,19 +3,29 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
 
+     Student student;
+
+     @RequestMapping("view_student")
+     public String viewStudent(Model model) {
+         model.addAttribute("student", student);
+         return "view_student";
+     }
+
     @RequestMapping("/new_student")
     public String newStudent(Model model) {
         System.out.println(Grade.values());
         model.addAttribute("grades", Grade.values());
+
         return "new_student";
     }
 
-    @RequestMapping("/create_student")
+    @RequestMapping(path = "/create_student", method = RequestMethod.POST)
     public String createStudent(@RequestParam(value="first_name") String firstName,
                                 @RequestParam(value="last_name")String lastName,
                                 @RequestParam(value="grade")Grade grade, Model model) {
@@ -25,12 +35,13 @@ public class StudentController {
             student.setFirstName(firstName);
             student.setLastName(lastName);
             student.setGrade(grade);
+            this.student = student;
 
 
 
         /* add the student to the model that will be used by the view_student html file */
         model.addAttribute("Student", student);
 
-        return "view_student";
+        return "redirect:/view_student.html";
     }
 }

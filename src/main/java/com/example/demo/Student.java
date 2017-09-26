2e7be044698644799593881bf8f0214370acb0e3
Evan Coakley <evancoakley1@gmail.com>
2017-09-26 15:53:41 -0500

    initial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class Student {
+    String firstName;
+    String lastName;
+    Grade grade;
+
+    public Student() {}
+
+    public Student(String firstName, String lastName, Grade grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
